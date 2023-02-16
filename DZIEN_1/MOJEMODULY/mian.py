6d2dbe4912d1c22bd271be9e42bee61774c60079
albim72 <marcin.albiniak72@gmail.com>
2023-02-16 13:34:42 +0100

    Create mian.py

---
+++ DZIEN_1/MOJEMODULY/mian.py
@@ -0,0 +1,9 @@
+#import dane
+#import dane as dn
+from dane import nb as filie
+from dane import book as ksiazka
+
+print("__________czytanie bezpośrednie___________")
+
+print(filie)
+print(ksiazka)
