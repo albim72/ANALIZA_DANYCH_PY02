abcc85102febb9eb25eae46d2be09c1cedbf3c6c
albim72 <marcin.albiniak72@gmail.com>
2023-02-16 13:44:31 +0100

    Update mian.py

--- DZIEN_1/MOJEMODULY/mian.py
+++ DZIEN_1/MOJEMODULY/mian.py
@@ -2,8 +2,13 @@
 #import dane as dn
 from dane import nb as filie
 from dane import book as ksiazka
+from funkcje.bfunkcje import czytaj_liste, czytaj_slownik
 
 print("__________czytanie bezpośrednie___________")
 
 print(filie)
 print(ksiazka)
+
+print("__________czytanie za pomocą funkcji___________")
+czytaj_liste(filie)
+czytaj_slownik(ksiazka)
