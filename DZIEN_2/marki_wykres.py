c16d1fce8543c9bcedbcc2af9a029fe2bab6047b
albim72 <marcin.albiniak72@gmail.com>
2023-02-17 11:50:24 +0100

    Create marki_wykres.py

---
+++ DZIEN_2/marki_wykres.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+
+fig,ax = plt.subplots()
+marki = ['audi','toyota','mercedes','honda','kia','jeep','seat']
+sprzed = [12.7,28.9,9.8,16.8,23.5,7.8,29.3]
+
+bar_labels = list(marki)
+bar_colors = ['tab:red','tab:green','tab:red','tab:blue','tab:orange','tab:red','tab:green']
+
+ax.bar(marki,sprzed,label = bar_labels,color = bar_colors)
+ax.set_xlabel('sprzdaż wybranych marek')
+ax.set_ylabel('marki samochodów')
+ax.legend(title='marki')
+plt.show()
