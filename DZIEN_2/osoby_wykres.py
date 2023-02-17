4f36b5e677c4b6e6f2f28dc6b2d94b406b7cc085
albim72 <marcin.albiniak72@gmail.com>
2023-02-17 12:09:30 +0100

    Create osoby_wykres.py

---
+++ DZIEN_2/osoby_wykres.py
@@ -0,0 +1,29 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+osoby = ("Anna","Piotr","Olga","Roman")
+informacje = {
+    'wiek':(34,56,23,45),
+    'waga':(56.8,89.6,60.3,103.6),
+    'wzrost':(162,172,170,189)
+    
+}
+
+x = np.arange(len(osoby))
+width = 0.25
+multiplier = 0
+
+fig, ax = plt.subplots(constrained_layout = True)
+for attribute,mesurement in informacje.items():
+    offset = width*multiplier
+    rects = ax.bar(x+offset,mesurement,width,label=attribute)
+    ax.bar_label(rects,padding=5)
+    multiplier += 1
+
+ax.set_ylabel("cecha")
+ax.set_title("wybrane informacje na temat osób...")
+ax.set_xticks(x+width,osoby)
+ax.legend(loc='upper left',ncols=3)
+ax.set_ylim(0,200)
+
+plt.show()
